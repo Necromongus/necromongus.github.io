9e4eaf35b6be6d283579dfe42c95410e464b317d
Sergey <necromongus@gmail.com>
2019-07-08 21:41:06 +0300

    added mobile ver

--- js/main.js
+++ js/main.js
@@ -4,19 +4,23 @@ function castleStart(){
 
     fill.addEventListener('dragstart', dragStart);
     fill.addEventListener('dragend', dragEnd);
+    fill.addEventListener("touchstart", handleStart);
+    fill.addEventListener("touchend", handleEnd);
+    fill.addEventListener("touchcancel", handleCancel);
+    // fill.addEventListener("touchmove", handleMove);
 
     for (const empty of empties) {
     empty.addEventListener('dragover', dragOver);
     empty.addEventListener('dragenter', dragEnter);
     empty.addEventListener('dragleave', dragLeave);
     empty.addEventListener('drop', dragDrop);
+    empty.addEventListener("touchmove", handleMove);
     }
 
     function dragStart() {
     fillClasses = this.closest("td").classList;
     this.className += ' hold';
     setTimeout(() => (this.className = 'invisible'), 0);
-
     }
 
     function dragEnd() {
@@ -44,6 +48,30 @@ function castleStart(){
         this.append(fill);
     } 
     }
+
+    function handleStart() {
+        fillClasses = this.closest("td").classList;
+        this.className += ' hold';
+        // setTimeout(() => (this.className = 'invisible'), 0);
+    }
+
+    function handleEnd() {
+        this.className = 'fill';
+    }
+
+    function handleCancel() {
+        this.className = 'fill';
+    }
+
+    function handleMove() {
+        if (this.classList[1] === fillClasses[1] || this.classList[2] === fillClasses[2]) {
+            this.classList.remove("hovered")  
+            this.append(fill);
+        } 
+    }
+
+
+
 }
 
 castleStart();
