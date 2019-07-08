a139bc37fdd9a2d110ad698ccd01fb545b625d84
Sergey <necromongus@gmail.com>
2019-07-09 00:55:56 +0300

    alpha ver 0.1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 function castleStart(){
     const fill = document.querySelector('.fill');
     const empties = document.querySelectorAll('.empty');
+    var fillClasses;
 
     fill.addEventListener('dragstart', dragStart);
     fill.addEventListener('dragend', dragEnd);
     fill.addEventListener("touchstart", handleStart);
     fill.addEventListener("touchend", handleEnd);
     fill.addEventListener("touchcancel", handleCancel);
-    // fill.addEventListener("touchmove", handleMove);
+    fill.addEventListener("touchmove", handleMove);
 
     for (const empty of empties) {
     empty.addEventListener('dragover', dragOver);
     empty.addEventListener('dragenter', dragEnter);
     empty.addEventListener('dragleave', dragLeave);
     empty.addEventListener('drop', dragDrop);
-    empty.addEventListener("touchmove", handleMove);
+    //empty.addEventListener("touchmove", handleMove);
     }
 
     function dragStart() {
@@ -55,19 +56,21 @@ function castleStart(){
         // setTimeout(() => (this.className = 'invisible'), 0);
     }
 
-    function handleEnd() {
+    function handleEnd(e) {
         this.className = 'fill';
+        var myLocation = event.changedTouches[0];
+        var realTarget = document.elementFromPoint(myLocation.clientX, myLocation.clientY);
+        if (realTarget.classList[1] === fillClasses[1] || realTarget.classList[2] === fillClasses[2]) {
+            realTarget.append(fill);
+        } 
     }
 
     function handleCancel() {
         this.className = 'fill';
     }
 
-    function handleMove() {
-        if (this.classList[1] === fillClasses[1] || this.classList[2] === fillClasses[2]) {
-            this.classList.remove("hovered")  
-            this.append(fill);
-        } 
+    function handleMove(e) {
+        fillClasses = fill.closest("td").classList;
     }
 
 
