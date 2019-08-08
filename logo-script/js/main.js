15ef9ca95df98f4df2c426c997e5fbf118a7aa71
Sergey <necromongus@gmail.com>
2019-08-08 14:33:49 +0300

    added codabra-quest

--- logo-script/js/main.js
+++ logo-script/js/main.js
@@ -2,6 +2,14 @@ const fb = document.querySelector('img[data-original="https://static.tildacdn.co
 const vk = document.querySelector('img[data-original="https://static.tildacdn.com/tild6534-6137-4334-b039-636138393039/vk-social-network-lo.png"]');
 const ig = document.querySelector('img[data-original="https://static.tildacdn.com/tild3132-3837-4138-a335-626637633330/instagram-logo.png"]');
 
+const cfb = document.querySelector('img[data-original="https://static.tildacdn.com/tild3336-3464-4535-a532-636530656433/facebook-logo_2.png"]');
+const cvk = document.querySelector('img[data-original="https://static.tildacdn.com/tild3164-3061-4736-b032-376534633262/vk-social-network-lo.png"]');
+const cig = document.querySelector('img[data-original="https://static.tildacdn.com/tild3739-3565-4833-a539-346235363739/instagram_1.png"]');
+
+cfb.style="display:none";
+cvk.style="display:none";
+cig.style="display:none";
+
 fb.addEventListener('mouseover', fbMouseOver);
 fb.addEventListener('mouseout', fbMouseOut);
 vk.addEventListener('mouseover', vkMouseOver);
@@ -11,7 +19,7 @@ ig.addEventListener('mouseout', igMouseOut);
 
 function fbMouseOver() {
     console.log('enter');
-    this.src = "https://facebookbrand.com/wp-content/themes/fb-branding/assets/images/fb-logo.png?v2";
+    this.src = "https://static.tildacdn.com/tild3336-3464-4535-a532-636530656433/facebook-logo_2.png";
 }
 
 function fbMouseOut() {
@@ -21,7 +29,7 @@ function fbMouseOut() {
 
 function vkMouseOver() {
     console.log('enter');
-    this.src = "http://pngimg.com/uploads/vkontakte/vkontakte_PNG24.png";
+    this.src = "https://static.tildacdn.com/tild3164-3061-4736-b032-376534633262/vk-social-network-lo.png";
 }
 
 function vkMouseOut() {
@@ -31,7 +39,7 @@ function vkMouseOut() {
 
 function igMouseOver() {
     console.log('enter');
-    this.src = "https://hire10.net/wp-content/uploads/2019/01/IG-logo-.png";
+    this.src = "https://static.tildacdn.com/tild3739-3565-4833-a539-346235363739/instagram_1.png";
 }
 
 function igMouseOut() {
